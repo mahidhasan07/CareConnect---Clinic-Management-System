0a0bacca7225f40646a93a3edc1ace603619d78e
mahidhasan07 <mahidhasanremon@gmail.com>
2026-01-06 00:34:37 +0600

    index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-// 1. Check if the user is already logged in
+// 1. Check if user is logged in
 if (isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in'] === true) {
     
-    // 2. Redirect to the specific dashboard based on their role
+    // Redirect to Dashboard based on Role
     if ($_SESSION['role'] === 'admin') {
         header("Location: View/admin_dashboard.php");
     } elseif ($_SESSION['role'] === 'doctor') {
@@ -12,13 +12,12 @@ if (isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in'] === true) {
     } elseif ($_SESSION['role'] === 'patient') {
         header("Location: View/patient_dashboard.php");
     } else {
-        // Fallback if role is undefined
         header("Location: View/login.php");
     }
 
 } else {
-    // 3. If not logged in, go to the Login Page
-    header("Location: View/login.php");
+    // 2. If NOT logged in, go to the Home Page (Landing Page)
+    header("Location: View/home.php");
 }
 
 exit();
