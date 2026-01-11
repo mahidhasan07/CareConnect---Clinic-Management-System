03b31a68e0d07f0e6621545e41c7da8a90e83b68
mahidhasan07 <mahidhasanremon@gmail.com>
2026-01-12 02:55:16 +0600

    new session auth add

--- Controller/session_auth.php
+++ Controller/session_auth.php
@@ -1,17 +1,21 @@
 <?php
 session_start();
 
+// 1. Check if user is logged in
 if (!isset($_SESSION['is_logged_in'])) {
     header("Location: ../View/login.php");
     exit();
 }
 
+// 5 min for testing. 
 $timeout_duration = 300;
 
+// 3. Check if the session has expired
 if (isset($_SESSION['last_time'])) {
     $elapsed_time = time() - $_SESSION['last_time'];
     
     if ($elapsed_time >= $timeout_duration) {
+        // Session expired
         session_unset();
         session_destroy();
         header("Location: ../View/login.php?msg=timeout");
@@ -19,5 +23,6 @@ if (isset($_SESSION['last_time'])) {
     }
 }
 
+// 4. Update the "Last Activity" time stamp
 $_SESSION['last_time'] = time();
 ?>
